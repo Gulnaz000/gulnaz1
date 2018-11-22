e1fef0eb42bc7e74d6828aa54a662218cc60b2f8
Gulnaz000 <44257793+Gulnaz000@users.noreply.github.com>
2018-11-22 11:42:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+﻿//alert('Ehhhf!');
+function f()
+{
+//alert(document.getElementById('b2').borderColor);
+document.getElementById("b2").borderColor="blue";
+}
+function g()
+{
+//alert(document.getElementById("b2").borderColor);
+document.getElementById("b2").borderColor="yellow";
+}
